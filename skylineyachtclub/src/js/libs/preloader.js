5a3dbd0326318e3ec2763957903a6d43aeb9d31c
Zhurin Vladislav <102624539+ZhurinVladislav@users.noreply.github.com>
2024-03-24 00:33:11 +0300

    final changes in the SkyLine project

--- skylineyachtclub/src/js/libs/preloader.js
+++ skylineyachtclub/src/js/libs/preloader.js
@@ -1,1 +1,21 @@
-document.querySelector('body').classList.add('loading');
+const preloader = html => {
+  const preloaderWrapper = document.getElementById('preloader');
+
+  if (!preloaderWrapper) return;
+
+  setTimeout(() => {
+    preloaderWrapper.classList.add('loading');
+  }, 200);
+
+  setTimeout(() => {
+    preloaderWrapper.remove();
+    html.classList.remove('pre-hidden');
+  }, 400);
+};
+
+const html = document.querySelector('html');
+html.classList.add('pre-hidden');
+
+window.addEventListener('load', () => {
+  preloader(html);
+});
